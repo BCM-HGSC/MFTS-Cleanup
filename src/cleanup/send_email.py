c4c39de423a294f01b020ae745bce99251fe11f6
Sarah Ponce <sarah_ponce@outlook.com>
2022-04-14 12:34:16 -0500

    setting first warning as its own text file

--- src/cleanup/send_email.py
+++ src/cleanup/send_email.py
@@ -1,6 +1,6 @@
 from logging import warning
-import os
-import smtplib
+import os # for interacting with operating system
+import smtplib 
 from email.message import EmailMessage
 
 EMAIL_SENDER = os.environ.get('EMAIL_SENDER')
@@ -11,19 +11,8 @@ first_warning['Subject'] = "Directory Cleanup Warning"
 first_warning['From'] = EMAIL_SENDER
 first_warning['To'] = os.environ.get('EMAIL_RECEIVER') # TODO figure out how to obtain this email that's not 'EMAIL_ADDRESS'
 
+#first_warning.set_content(first_email.txt)
 
-first_warning.set_content('Hello, this message is to warn you all files under the folder f'{[link given from directory]} as f'{[/share/share/mfts/private/{requestor}/rt f'{######}]}} has been shared with the collaborator.
- 1 MFTS account has been created. Login information and instructions to download the data has been emailed to the collaborator directly.
- In order to conserve our finite data transfer storage, this data will be kept live for 4 weeks and then purged. Please let us know promptly if the collaborator encounters any problems downloading and validating this data.
- Thank you,
- HGSC
- Baylor College of Medicine
- ')
-
-
-
- with smtplib.smtplib.SMTP_SSL('smtp.rt.hgsc.bcm.edu/', 465) as smtp:
+with smtplib.smtplib.SMTP_SSL('smtp.rt.hgsc.bcm.edu/', 465) as smtp:
      smtp.login(EMAIL_SENDER, EMAIL_PASSWORD)
      smtp.send_message(first_warning)
-
-
