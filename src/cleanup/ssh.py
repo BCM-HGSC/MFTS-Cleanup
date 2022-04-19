15e98c5cf65e38e06f5cee3d691a326c5dac0dcf
Sarah Ponce <sarah_ponce@outlook.com>
2022-04-19 13:00:38 -0500

    ssh.py

---
+++ src/cleanup/ssh.py
@@ -0,0 +1,13 @@
+import paramiko
+
+# making a connection to the server
+
+# initialize the SSH client
+client = paramiko.SSHClient()
+# add to known hosts
+client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+try:
+    client.connect(hostname=hostname, username=username, password=password)
+except:
+    print("[!] Cannot connect to the SSH Server")
+    exit()
