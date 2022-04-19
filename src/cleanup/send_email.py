764bac56692140189322ccd9500ab4099b9a5bd0
Sarah Ponce <sarah_ponce@outlook.com>
2022-04-19 10:38:20 -0500

    user path input

--- src/cleanup/send_email.py
+++ src/cleanup/send_email.py
@@ -1,28 +1,5 @@
-# from logging import warning
-# import os # for interacting with operating system
-# import smtplib 
-# from email.message import EmailMessage
-
-
-# EMAIL_SENDER = os.environ.get('EMAIL_SENDER')
-# EMAIL_PASSWORD = os.environ.get('EMAIL_PASSWORD')
-# EMAIL_RECEIVER = os.environ.get('EMAIL_RECEIVER')
-# first_warning = EmailMessage()
-# first_warning['Subject'] = "Directory Cleanup Warning"
-# first_warning['From'] = EMAIL_SENDER
-# first_warning['To'] = os.environ.get('EMAIL_RECEIVER') #TOD0 figure out how to obtain this email that's not 'EMAIL_ADDRESS'
-
-# #first_warning.set_content(first_email.txt)
-
-# with smtplib.smtplib.SMTP_SSL('smtp.rt.hgsc.bcm.edu/', 465) as smtp:
-#      smtp.login(EMAIL_SENDER, EMAIL_PASSWORD)
-#      smtp.send_message(first_warning)
-
-# argparse is a command line argument processing
-
 # import library
 import argparse
-import sys
 import os
 # from pathlib import Path
 
@@ -41,12 +18,12 @@ parser.add_argument('--email_send',type=str)
 args = parser.parse_args()
 
 # user input for directory path
-user_input = input("Enter directory path: ")
-assert os.path.exists(user_input), "No path found at this location: " + str(user_input)
-f = open(user_input, 'r+')
-print("Path sucessfully loaded")
-# TODO figure out what the file location requrires
-f.close()
+import os
+
+path = input("enter filepath: ")
+
+for f in os.listdir(os.path.expanduser(path)):
+    print(f)
 
 
 
