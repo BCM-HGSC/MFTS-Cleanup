d6bbe9215558ad6b68e9dbc2b45e8531bacc97d9
Sarah Ponce <sarah_ponce@outlook.com>
2022-04-19 15:23:21 -0500

    paramiko probs

--- src/cleanup/ssh.py
+++ src/cleanup/ssh.py
@@ -1,7 +1,8 @@
 import paramiko
 
+
 ssh = paramiko.SSHClient()
 
-ssh.connect('10.70.12.36', port=22, username='sug-login1',password= )
+ssh.connect('10.70.12.36', port=22, username='sug-login1')
 
 
