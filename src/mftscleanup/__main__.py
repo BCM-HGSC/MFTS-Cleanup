8e0223d737c0510ce94000aa979fb6f5eb13f6fe
Sarah Ponce <sarah_ponce@outlook.com>
2022-04-27 15:43:15 -0500

    adding dir_path function, loading config file, and adding parser arguments for CL

--- src/mftscleanup/__main__.py
+++ src/mftscleanup/__main__.py
@@ -4,10 +4,13 @@ a script by running:
 `python -m mftscleanup [args...]`
 """
 
-from sys import argv, path
+import argparse,os
+
+from sys import argv
 from textwrap import dedent
 
 from addict import Dict
+import yaml
 
 from . import __version__
 from .cleanup import auto_cleanup, new_share
@@ -46,14 +49,35 @@ def parse_register_command_line():
     """
     `register-new-share CONFIG_FILE_PATH RT_NUMBER SHARE_DIRECTORY_PATH EMAIL [EMAIL]...`
     """
-    pass  # TODO
-    return Dict()
+    parser= argparse.ArgumentParser(description="Registering a new share")
+    parser.add_argument('CONFIG_FILE_PATH',type=str)
+    parser.add_argument('RT_NUMBER',type=str)
+    parser.add_argument('SHARE_DIRECTORY_PATH', type= dir_path)
+    parser.add_argument('EMAIL', type=str)
+    args = parser.parse_args()
+
+    print("the inputs are:")
+    for arg in vars(args):
+        print("{} is {}".format(arg, getattr(args, arg)))
+
+    return args
+    # return Dict()
 
 
 def load_config(config_file_path):
-    pass  # TODO
-    return Dict()
+    with open('src/mftscleanup/config.yaml', mode= "r+") as config_file_path:
+        for info in config_file_path:
+            return info
+    return Dict(yaml.safe_load(config_file_path))
 
 
 if __name__ == "__main__":
     main()
+
+def dir_path(string):
+    if os.path.isdir(string):
+        return string
+    else:
+        raise NotADirectoryError(string)
+
+
