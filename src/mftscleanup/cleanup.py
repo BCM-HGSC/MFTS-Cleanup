775816829fdf5edb1adfa87bf6d1923b0c214f13
Sarah Ponce <sarah_ponce@outlook.com>
2022-05-23 14:14:46 -0500

    obtaining all files at active/*_initial.yaml

--- src/mftscleanup/cleanup.py
+++ src/mftscleanup/cleanup.py
@@ -2,8 +2,9 @@
 Implements the registration and automatic cleanup of shares.
 Operates just below command line parsing.
 """
-from pathlib import Path
 
+from os import access
+import pathlib
 from yaml import dump
 
 def new_share(metadata_root, rt_number, share_directory, email_addresses, no_of_files, t_file_size):
@@ -19,19 +20,31 @@ def new_share(metadata_root, rt_number, share_directory, email_addresses, no_of_
 
 
     )
-    destination = Path(metadata_root) / "active" / f"{rt_number}_initial.yaml"
+    destination = pathlib.Path(metadata_root) / "active" / f"{rt_number}_initial.yaml"
     directory = destination.parent
     directory.mkdir(parents=True, exist_ok=True)
     destination.write_text(dump(payload), encoding="UTF-8")  
+   
+
+def new_cleanup(config_file_path):
+    active_dir = config_file_path.parent
+    top = pathlib.Path(active_dir)
+    for p in top.rglob("*"):
+        if p.is_file():
+            print(p)
+
+ 
 
 
-def new_cleanup(metadata_root, rt_number):
-    payload = dict(
-        rt_number = int(rt_number)
-    )
-    destination = Path(metadata_root) / "active" / f"{rt_number}_initial.yaml"
-    directory = destination.parent
-    directory.mkdir(parents=True, exist_ok=True)
-    destination.write_text(dump(payload), encoding="UTF-8")  
+
+        
+    pass
+    # payload = dict(
+    # #     rt_number = int(rt_number)
+    # )
+    # destination = Path(metadata_root) / "active" / f"{rt_number}_first_email.yaml"
+    # directory = destination.parent
+    # directory.mkdir(parents=True, exist_ok=True)
+    # destination.write_text(dump(payload), encoding="UTF-8")  
 
     
