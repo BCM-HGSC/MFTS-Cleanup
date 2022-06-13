84728128bbfe7f15ca7ccd28b5047898b7455eb5
Walker Hale IV <walker.hale.iv@gmail.com>
2022-06-13 14:13:06 -0500

    Move responsibility for scanning directory down

--- src/mftscleanup/cleanup.py
+++ src/mftscleanup/cleanup.py
@@ -6,6 +6,7 @@ Operates just below command line parsing.
 
 from datetime import date, timedelta
 import pathlib
+from os.path import getsize
 
 from addict import Dict
 from yaml import dump, safe_load
@@ -16,8 +17,6 @@ def new_share(
     rt_number,
     share_directory: pathlib.Path,
     email_addresses,
-    no_of_files,
-    t_file_size,
     start_date=None,
 ):
     """
@@ -25,6 +24,7 @@ def new_share(
     """
     if start_date is None:
         start_date = date.today()
+    no_of_files, t_file_size = get_directory_totals(share_directory)
     payload = dict(
         rt_number=int(rt_number),
         share_directory=str(share_directory),
@@ -37,6 +37,15 @@ def new_share(
     directory = destination.parent
     directory.mkdir(parents=True, exist_ok=True)
     destination.write_text(dump(payload), encoding="UTF-8")
+
+
+def get_directory_totals(top: pathlib.Path):
+    num_files = total_size = 0
+    for p in list(top.rglob("*")):
+        if p.is_file():
+            num_files += 1
+            total_size += getsize(p)
+    return num_files, total_size
 
 
 def process_active_shares(metadata_root, from_address, email_host):
