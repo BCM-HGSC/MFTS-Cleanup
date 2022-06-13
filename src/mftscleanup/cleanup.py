68d3b340b6d9b31241e68dd5a0be8593aa482864
Walker Hale IV <walker.hale.iv@gmail.com>
2022-06-13 14:14:32 -0500

    Assert argument type

--- src/mftscleanup/cleanup.py
+++ src/mftscleanup/cleanup.py
@@ -24,6 +24,7 @@ def new_share(
     """
     if start_date is None:
         start_date = date.today()
+    assert share_directory.is_dir(), share_directory
     no_of_files, t_file_size = get_directory_totals(share_directory)
     payload = dict(
         rt_number=int(rt_number),
