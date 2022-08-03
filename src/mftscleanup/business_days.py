7b34171a4005fdfb363f923f0dc5439bf0ea1394
Sarah Ponce <sarah_ponce@outlook.com>
2022-08-03 11:34:21 -0500

    business days (wip)

--- src/mftscleanup/business_days.py
+++ src/mftscleanup/business_days.py
@@ -4,9 +4,22 @@ future. A data is a business day if it is neither a weekend nor a US observed ho
 """
 
 from datetime import date, timedelta
+import holidays
 
-
-ONE_DAY = timedelta(days=1)
 
 def add_business_days(start: date, num_businessdays: int) -> date:
-    raise NotImplementedError  # TODO
+    business_days_weekends_holidays = num_businessdays #TODO: wip
+    current_date = start
+    ONEDAY = timedelta(days=1)
+    while business_days_weekends_holidays > 0:
+        current_date = current_date.today()  #+= timedelta(days=1)
+        weekday = current_date.weekday() 
+        if weekday >=5: # sunday = 6 
+            ONEDAY # wip
+            #non weekend
+        for h in holidays.USA(h).items():
+            ONEDAY # wip
+            # holiday
+        else:
+            continue # businessday (wip)
+    return current_date
