d92859a4a765b43be8768497f01ef32083fa39e8
Walker Hale IV <walker.hale.iv@gmail.com>
2022-08-18 21:13:48 -0500

    Overhaul CLI
    - Move responsibility for getting today's date up.
    - Now require metadata_root directory instead of config_file path.
    - Use consistent names for entry points and associated functions.
    - Pass around an argv reference for testability.
    - Reorder functions.
    - More testing.

--- tests/test_register_new_share.py
+++ tests/test_register_new_share.py
@@ -1,12 +1,18 @@
 from datetime import date
-from pathlib import Path
+from shutil import which
+from sys import executable
+from subprocess import run
 from textwrap import dedent
 
+from mftscleanup import __main__
 from mftscleanup import cleanup
 from helpers import FakeShare
 
 
 def test_new_share_happy_path(rt1234: FakeShare):
+    """
+    Invoke the cleanup.new_share function from within the pytest process.
+    """
     # Phase 1: setup.
     pass  # Alreadly handled in rt1234.
     # Phase 2: run the code you are testing.
@@ -25,6 +31,108 @@ def test_new_share_happy_path(rt1234: FakeShare):
         email_addresses:
         - fake@fake.com
         initial_date: '2020-01-01'
+        num_bytes: {rt1234.num_bytes}
+        num_files: {rt1234.num_files}
+        share_directory: {rt1234.share_root}
+        share_id: {rt1234.share_id}
+        state: initial
+        """
+    )
+    assert yaml_path.read_text() == EXPECTED_YAML
+
+
+def test_new_share_via_main(rt1234: FakeShare):
+    """
+    Invoke the top-level main function from within the pytest process.
+    """
+    today_str = date.today().isoformat()
+    argv = [
+        "new",
+        str(rt1234.scenario.metadata_root),
+        "1234",
+        str(rt1234.share_root),
+        "fake@fake.com",
+    ]
+    __main__.main(argv)
+    yaml_path = rt1234.scenario.active / "rt1234_0000.yaml"
+    assert yaml_path.is_file()
+    EXPECTED_YAML = dedent(
+        f"""\
+        email_addresses:
+        - fake@fake.com
+        initial_date: '{today_str}'
+        num_bytes: {rt1234.num_bytes}
+        num_files: {rt1234.num_files}
+        share_directory: {rt1234.share_root}
+        share_id: {rt1234.share_id}
+        state: initial
+        """
+    )
+    assert yaml_path.read_text() == EXPECTED_YAML
+
+
+def test_new_share_python_command(rt1234: FakeShare):
+    """
+    Invoke the top-level main function in a new Python process by using:
+    `python -m mftscleanup new ...`
+    """
+    today_str = date.today().isoformat()
+    print(executable)
+    assert executable
+    command = [
+        executable,
+        "-m",
+        "mftscleanup",
+        "new",
+        str(rt1234.scenario.metadata_root),
+        "1234",
+        str(rt1234.share_root),
+        "fake@fake.com",
+    ]
+    completed_process = run(command)
+    assert completed_process.returncode == 0
+    yaml_path = rt1234.scenario.active / "rt1234_0000.yaml"
+    assert yaml_path.is_file()
+    EXPECTED_YAML = dedent(
+        f"""\
+        email_addresses:
+        - fake@fake.com
+        initial_date: '{today_str}'
+        num_bytes: {rt1234.num_bytes}
+        num_files: {rt1234.num_files}
+        share_directory: {rt1234.share_root}
+        share_id: {rt1234.share_id}
+        state: initial
+        """
+    )
+    assert yaml_path.read_text() == EXPECTED_YAML
+
+
+def test_new_share_shell_command(rt1234: FakeShare):
+    """
+    Invoke the register_new_share function in a new Python process by using:
+    `/path/to/register-new-share ...`
+    """
+    today_str = date.today().isoformat()
+    executable_script_path = which("register-new-share")
+    print(executable_script_path)
+    assert executable_script_path
+    command = [
+        executable_script_path,
+        str(rt1234.scenario.metadata_root),
+        "1234",
+        str(rt1234.share_root),
+        "fake@fake.com",
+    ]
+    completed_process = run(command)
+    assert completed_process.returncode == 0
+    yaml_path = rt1234.scenario.active / "rt1234_0000.yaml"
+    assert yaml_path.is_file()
+    EXPECTED_YAML = dedent(
+        f"""\
+        email_addresses:
+        - fake@fake.com
+        initial_date: '{today_str}'
         num_bytes: {rt1234.num_bytes}
         num_files: {rt1234.num_files}
         share_directory: {rt1234.share_root}
