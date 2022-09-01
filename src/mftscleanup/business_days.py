86864dc2058769b1f216e6c645eace49e176fd7a
Sarah Ponce <sarah_ponce@outlook.com>
2022-09-01 15:02:37 -0500

    adding department holidays to add_business_days

--- src/mftscleanup/business_days.py
+++ src/mftscleanup/business_days.py
@@ -22,12 +22,8 @@ def add_business_days(start: date, num_businessdays: int) -> date:
         current_date += ONE_DAY
         if current_date.weekday() >= 5:  # sunday = 6
             continue  # weekend, so nothing to do
-        if current_date in us_holidays:
-            continue  # businessday (wip)
-        if current_date.month == 11:  # November
-            if current_date.weekday() == 4:  # Friday
-                if 23 <= current_date.day <= 29:  # In range of Black Friday
-                    continue
+        if current_date in dep_holidays: 
+           continue # working day
         remaining_business_days -= 1
     return current_date
 
@@ -38,7 +34,7 @@ class departmental_Holidays(holidays.US):
         # Columbus day is not observed
         self.pop_named("Columbus Day")
         # removing Veterans day because we don't observe 
-        self.pop_named("Veteran's Day")
+        self.pop_named("Veterans Day")
         # observe Good Friday as per hgsc google cal
         self[E(year) + RD(weekday = FR(-1))] = "Good Friday" 
         # also observe black friday
