bbf72a48a3b1552b666b0cf9b4264c2177a1b969
Sarah Ponce <sarah_ponce@outlook.com>
2022-09-13 14:13:58 -0500

    WIP: holiday.constants, Juneteenth; Christmas Eve

--- src/mftscleanup/business_days.py
+++ src/mftscleanup/business_days.py
@@ -6,9 +6,17 @@ future. A data is a business day if it is neither a weekend nor a US observed ho
 from datetime import date, timedelta
 from dateutil.easter import easter as E
 from dateutil.relativedelta import relativedelta as RD
-from dateutil.relativedelta import TH, FR
+from dateutil.relativedelta import TH, FR, SA,SU
 
 import holidays
+from holidays.constants import (
+    SAT,
+    SUN, 
+    WEEKEND,
+    JUN,
+    DEC
+)
+
 
 
 ONE_DAY = timedelta(days=1)
@@ -39,6 +47,28 @@ class departmental_Holidays(holidays.US):
         self[E(year) + RD(weekday = FR(-1))] = "Good Friday" 
         # also observe black friday
         self[date(year, 11, 1) + RD(weekday=TH(+4)) + ONE_DAY] = "Black Friday"
-
+        
+        # Juneteenth Day
+        if year > 2022:
+            name = "Juneteenth National Independence Day"
+            self[date(year, JUN, 19)] = name
+            if self.observed and date(year, JUN, 19).weekday() == SAT:
+                self[date(year, JUN, 18)] = name + " (Observed)"
+            elif self.observed and date(year, JUN, 19).weekday() == SUN:
+                self[date(year, JUN, 20)] = name + " (Observed)"
+        self.pop_named("Juneteenth National Independence Day")
+        
+        
+        # Christmas Eve
+        if year < 2022:
+            name = "Christmas Eve"
+            self[date(year, DEC, 24)] = name
+            name = name + " (Observed)"
+            # If on Friday, observed on Thursday
+            if self.observed and date(year, DEC, 24).weekday() == SAT:
+                self[date(year, DEC, 24) + RD(days=-1)] = name
+            # If on Saturday or Sunday, observed on Friday
+            elif self.observed and date(year, DEC, 24).weekday() in WEEKEND:
+                self[date(year, DEC, 24) + RD(weekday=FR(-1))] = name
 
 dep_holidays = departmental_Holidays()
