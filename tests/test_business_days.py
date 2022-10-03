8adcca67aa9754f3f621616f0c223a3f1acb9210
Sarah Ponce <sarah_ponce@outlook.com>
2022-10-03 16:07:35 -0500

    fix 2020 juneteenth days bc memorial day

--- tests/test_business_days.py
+++ tests/test_business_days.py
@@ -4,7 +4,7 @@ from datetime import date as D
 from pytest import mark
 
 from mftscleanup.business_days import add_business_days
-
+from mftscleanup.business_days import dep_holidays
 
 """
 For reference:
@@ -80,14 +80,78 @@ def test_black_friday():
 def test_good_friday():
     assert add_business_days(D(2020, 4, 7), 4) == D(2020, 4, 14)
 
+
 """
       May 2020        
 Su Mo Tu We Th Fr Sa  
                 1  2  
  3  4  5  6  7  8  9  
 10 11 12 13 14 15 16  
-17 18 19 20 21 22 23  
-24 25 26 27 28 29 30  
+17 18 19 20 21 ST 23  
+24 25 26 27 28 MD 30  
+31                    
+
+        June 2020        
+Su Mo Tu We Th Fr Sa  
+    1  2  3  4  5  6  
+ 7  8  9 10 11 12 13  
+14 15 16 SE 18 JT 20  
+21 22 23 24 25 26 27  
+28 29 30           
+
+
+"""
+
+"""
+      May 2020        
+Su Mo Tu We Th Fr Sa  
+                1  2  
+ 3  4  5  6  7  8  9  
+10 11 12 13 14 15 16  
+17 18 19 20 21 ST 23  
+24 25 26 27 28 MD 30  
+31                    
+
+        June 2020        
+Su Mo Tu We Th Fr Sa  
+    1  2  3  4  5  6  
+ 7  8  9 10 11 12 13  
+14 15 16 SE 18 JT 20  
+21 22 23 24 25 26 27  
+28 29 30           
+
+
+"""
+
+"""
+      May 2020        
+Su Mo Tu We Th Fr Sa  
+                1  2  
+ 3  4  5  6  7  8  9  
+10 11 12 13 14 15 16  
+17 18 19 20 21 ST 23  
+24 25 26 27 28 MD 30  
+31                    
+
+        June 2020        
+Su Mo Tu We Th Fr Sa  
+    1  2  3  4  5  6  
+ 7  8  9 10 11 12 13  
+14 15 16 SE 18 JT 20  
+21 22 23 24 25 26 27  
+28 29 30           
+
+
+"""
+
+"""
+      May 2020        
+Su Mo Tu We Th Fr Sa  
+                1  2  
+ 3  4  5  6  7  8  9  
+10 11 12 13 14 15 16  
+17 18 19 20 21 ST 23  
+24 25 26 27 28 MD 30  
 31                    
 
         June 2020        
@@ -103,11 +167,43 @@ Su Mo Tu We Th Fr Sa
 
 def test_Juneteenth_2020():
     # ticket starts on 5/22/2020
-    assert add_business_days(D(2020,6,12),3) == D(2020,6,17) # second email test 
-    assert add_business_days(D(2020,6,17),3) == D(2020,6,22) # Final email test
-    assert add_business_days(D(2020,6,22),1) == D(2020,6,23) # cleanup
+    assert add_business_days(D(2020,5,22),15) == D(2020,6,15) # first email test
+    assert add_business_days(D(2020,6,15),3) == D(2020,6,18) # second email test 
+    assert add_business_days(D(2020,6,18),3) == D(2020,6,23) # Final email test
+    assert add_business_days(D(2020,6,23),1) == D(2020,6,24) # cleanup
 
-#   2021 & 2022 Juneteenth fall on Sat and Sun. Next test will be for 2023
+"""
+      May 2020        
+Su Mo Tu We Th Fr Sa  
+                1  2  
+ 3  4  5  6  7  8  9  
+10 11 12 13 14 15 16  
+17 18 19 20 21 ST 23  
+24 25 26 27 28 MD 30  
+31                    
+
+        June 2020        
+Su Mo Tu We Th Fr Sa  
+    1  2  3  4  5  6  
+ 7  8  9 10 11 12 13  
+14 15 16 17 SE JT 20  
+21 22 23 24 25 26 27  
+28 29 30           
+
+
+"""
+
+
+
+
+#   2021 & 2022 Juneteenth fall on Sat and Sun. Next test will be for 2023. Memorial day falls on May 29th 
+
+
+def test_Juneteenth_2023():
+    
+# ticket starts on 5/22/2023
+    assert add_business_days(D(2023,6,12),5) == D(2023,6,20) # first email
+    assert add_business_days(D(2023,6,20),3) == D(2023,6,23) # second email
 """
 
       May 2023        
@@ -116,7 +212,7 @@ Su Mo Tu We Th Fr Sa
  7  8  9 10 11 12 13  
 14 15 16 17 18 19 20  
 21 22 23 24 25 26 27  
-28 29 30 31   
+28 MD 30 31   
 
      June 2023        
 Su Mo Tu We Th Fr Sa  
@@ -127,8 +223,3 @@ Su Mo Tu We Th Fr Sa
 25 26 27 28 29 30     
                     
 """
-
-def test_Juneteenth_2023():
-# ticket starts on 5/22/2023
-    assert add_business_days(D(2023,6,12),5) == D(2023,6,20) # first email
-    assert add_business_days(D(2023,6,20),3) == D(2023,6,23) # second email
