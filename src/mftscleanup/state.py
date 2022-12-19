f390eda17ea2de7613f2822850f7927841ca77a9
Walker Hale IV <walker.hale.iv@gmail.com>
2022-12-19 14:10:32 -0600

    Rename locals for consistency across functions

--- src/mftscleanup/state.py
+++ src/mftscleanup/state.py
@@ -36,24 +36,24 @@ STATE_NAMES = [s.name for s in State]
 
 
 def get_transition(
-    start_state: State, start_state_date: date
+    current_state: State, current_state_start_date: date
 ) -> Tuple[Optional[State], Optional[date]]:
 
-    if start_state == State.initial:
+    if current_state == State.initial:
         number_of_business_days = 15
-    elif start_state == State.first_email:
+    elif current_state == State.first_email:
         number_of_business_days = 3
-    elif start_state == State.second_email:
+    elif current_state == State.second_email:
         number_of_business_days = 1
-    elif start_state == State.final_email:
+    elif current_state == State.final_email:
         number_of_business_days = 1
-    elif start_state == State.cleanup:
+    elif current_state == State.cleanup:
         return (None, None)
-    elif start_state == State.hold:
+    elif current_state == State.hold:
         return (None, None)
 
-    new_date = add_business_days(start_state_date, number_of_business_days)
-    new_state = start_state.next
+    new_date = add_business_days(current_state_start_date, number_of_business_days)
+    new_state = current_state.next
 
     return new_state, new_date
 
