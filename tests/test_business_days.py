17e2b96e31b8f22d9fc1c149d3161ab94b25b948
Sarah Ponce <sarah_ponce@outlook.com>
2023-01-09 13:32:37 -0600

    Whitespace

--- tests/test_business_days.py
+++ tests/test_business_days.py
@@ -80,22 +80,22 @@ def test_good_friday():
 
 
 """
-      May 2020        
-Su Mo Tu We Th Fr Sa  
-                1  2  
- 3  4  5  6  7  8  9  
-10 11 12 13 14 15 16  
-17 18 19 20 21 ST 23  
-24 25 26 27 28 MD 30  
-31                    
+      May 2020
+Su Mo Tu We Th Fr Sa
+                1  2
+ 3  4  5  6  7  8  9
+10 11 12 13 14 15 16
+17 18 19 20 21 ST 23
+24 25 26 27 28 MD 30
+31
 
-        June 2020        
-Su Mo Tu We Th Fr Sa  
-    1  2  3  4  5  6  
- 7  8  9 10 11 12 13  
-14 15 16 SE 18 JT 20  
-21 22 23 24 25 26 27  
-28 29 30           
+     June 2020
+Su Mo Tu We Th Fr Sa
+    1  2  3  4  5  6
+ 7  8  9 10 11 12 13
+14 15 16 SE 18 JT 20
+21 22 23 24 25 26 27
+28 29 30
 
 
 """
@@ -112,23 +112,22 @@ def test_Juneteenth_2020():
 #   2021 & 2022 Juneteenth fall on Sat and Sun. Next test will be for 2023. Memorial day falls on May 29th
 
 """
+      May 2023
+Su Mo Tu We Th Fr Sa
+    1  2  3  4  5  6
+ 7  8  9 10 11 12 13
+14 15 16 17 18 19 20
+21 22 23 24 25 26 27
+28 MD 30 31
 
-      May 2023        
-Su Mo Tu We Th Fr Sa  
-    1  2  3  4  5  6  
- 7  8  9 10 11 12 13  
-14 15 16 17 18 19 20  
-21 22 23 24 25 26 27  
-28 MD 30 31   
+     June 2023
+Su Mo Tu We Th Fr Sa
+             1  2  3
+ 4  5  6  7  8  9 10
+11 12 13 14 15 16 17
+18 JT 20 21 22 23 24
+25 26 27 28 29 30
 
-     June 2023        
-Su Mo Tu We Th Fr Sa  
-             1  2  3  
- 4  5  6  7  8  9 10  
-11 12 13 14 15 16 17  
-18 JT 20 21 22 23 24  
-25 26 27 28 29 30     
-                    
 """
 
 
